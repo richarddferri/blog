ecf0d1b78956c6c91906dcadf9be6c587ec2ac70
Richard Ferri <richard.ferri@gmail.com>
2014-02-05 20:49:27 -0500

    reverted changes

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
- config.assets.precompile += [
-'application.css',
-'application.js',
-'matrixoft_logo7.jpg'
-]
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
